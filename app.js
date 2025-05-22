56c6bfdee51f9cf21a41a7ac47dd96fd8f70497c
svalbuenaa <31705507+svalbuenaa@users.noreply.github.com>
2025-05-22 11:50:26 +0200

    Modify both app.js and index.html to adapt width

--- app.js
+++ app.js
@@ -2,48 +2,48 @@ console.log('Is this working?');
 
 let viz;
 
-//Add Share Link to Tableau Public in here
+// Tableau Public URL
 const url = "https://public.tableau.com/views/PRA2Visualization-SergioValbuena-40Yearsofcancerstudies/40YEARSOFCANCERSTUDIES?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
-const options = {
-    hideTabs: true,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    onFirstInteraction: function() {
-        workbook = viz.getWorkbook();
-        activeSheet = workbook.getActiveSheet();
-        console.log("My dashboard is interactive");
-    }
-};
 
-//create a function to generate the viz element
+function getOptions() {
+    return {
+        hideTabs: true,
+        width: vizContainer.offsetWidth,
+        height: vizContainer.offsetHeight,
+        onFirstInteraction: function () {
+            const workbook = viz.getWorkbook();
+            const activeSheet = workbook.getActiveSheet();
+            console.log("My dashboard is interactive");
+        }
+    };
+}
+
+// Create and load the viz
 function initViz() {
     console.log('Executing the initViz function!');
-    viz = new tableau.Viz(vizContainer, url, options);
+    viz = new tableau.Viz(vizContainer, url, getOptions());
 }
 
-// run the initViz function when the page loads
+// Reinitialize viz on window resize
+window.addEventListener("resize", () => {
+    if (viz) {
+        viz.dispose();
+    }
+    initViz();
+});
+
 document.addEventListener("DOMContentLoaded", initViz);
 
+// Export PDF
 const exportPDF = document.getElementById('exportPDF');
+exportPDF.addEventListener("click", () => {
+    viz.showExportPDFDialog();
+});
+
+// Export Image
 const exportImage = document.getElementById('exportImage');
-
-
-//click on the pdf button to generate pdf of dashboard
-function generatePDF() {
-    viz.showExportPDFDialog()
-}
-
-exportPDF.addEventListener("click", function () {
-    generatePDF();
-  });
-
-//click on image to generate image of dashboard
-function generateImage() {
-    viz.showExportImageDialog()
-}
-
-exportImage.addEventListener("click", function () {
-    generateImage();
-  });
+exportImage.addEventListener("click", () => {
+    viz.showExportImageDialog();
+});
